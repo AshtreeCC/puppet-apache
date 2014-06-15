aa26c222d79b5e1e071537da5063de893e9290d1
Andrew Nash <akahadaka@gmail.com>
2014-06-15 14:55:57 +0200

    Removed support for PHP-FPM

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,33 +1,13 @@
 class apache::install {
 
-    package { 'apache2-mpm-worker' :
+    package { 'apache2' :
         ensure => installed,
         require => Class['php']
-    }
-
-    package { 'libapache2-mod-fastcgi' :
-        ensure => installed,
-        require => Package['apache2-mpm-worker']
     }
 }
 
 class apache::configure {
     
-    exec {'a2enmod actions fastcgi alias':
-        command => '/usr/bin/a2enmod actions fastcgi alias',
-        require => Package['libapache2-mod-fastcgi']
-    }
-
-    file { '/etc/apache2/conf.d/php5-fpm.conf':
-        content => '
-            <IfModule mod_fastcgi.c>
-                AddHandler php5-fcgi .php
-                Action php5-fcgi /php5-fcgi
-                Alias /php5-fcgi /usr/lib/cgi-bin/php5-fcgi
-                FastCgiExternalServer /usr/lib/cgi-bin/php5-fcgi -host 127.0.0.1:9000 -pass-header Authorization
-            </IfModule>',
-        require => Exec['a2enmod actions fastcgi alias']
-    }
 }
 
 class apache::run {
@@ -50,7 +30,7 @@ define addServer( $site, $root ) {
         owner   => 'root',
         group   => 'root',
         mode    => '755',
-        require => Package['apache2-mpm-worker'],
+        require => Package['apache2'],
         notify  => Service['apache2']
     }
 
